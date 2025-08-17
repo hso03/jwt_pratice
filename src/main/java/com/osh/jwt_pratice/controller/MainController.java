bde5353bdde5ebe12646a7cccaee78aef9d0deb7
오성현 <fhqht303@naver.com>
2025-08-18 00:09:09 +0900

    feat : username, role 획득

--- src/main/java/com/osh/jwt_pratice/controller/MainController.java
+++ src/main/java/com/osh/jwt_pratice/controller/MainController.java
@@ -1,7 +1,13 @@
 package com.osh.jwt_pratice.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @RestController
 public class MainController {
@@ -9,7 +15,16 @@ public class MainController {
     @GetMapping("/")
     public String mainP() {
 
-        return "Main Controller";
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        return "Main Controller" + username + role;
     }
 
 }
