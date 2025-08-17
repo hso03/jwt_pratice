0882619d31449ee75ecfd9c394e70e895cb3b26d
오성현 <fhqht303@naver.com>
2025-08-17 21:58:14 +0900

    feat : CustomUserDetailsService.java 생성

---
+++ src/main/java/com/osh/jwt_pratice/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.osh.jwt_pratice.service;
+
+import com.osh.jwt_pratice.dto.CustomUserDetails;
+import com.osh.jwt_pratice.entity.UserEntity;
+import com.osh.jwt_pratice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if(userData != null) {
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
